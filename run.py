6fcf5b2a424dae924ac146e1af422dfe9fcdc4e3
Drops <142268342+DewDropsKnowledge@users.noreply.github.com>
2023-10-14 20:34:47 -0400

    Update run.py

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import time
 
 print('Article Scraper')
 url = input("Please input URL with article directory: ")
-quantity_url = input("Please input total amount of URLs wanted: ")
+quantity_url = int(input("Please input total amount of URLs wanted: "))
 
 # Use the urlScraper function to scrape article URLs and save them to a file
 urlScraper(url, quantity_url)
